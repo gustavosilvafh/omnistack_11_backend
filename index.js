b0c584855e16ae498fccbc16960785552fe91826
Gustavo Silva <gustavo@MacBook-Pro-de-Gustavo.local>
2020-03-24 09:20:13 -0300

    fixing code

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 
 const app = express();
-app.use(express.json);
+app.use(express.json());
 
 
-app.post('/contato', function(req, res){
+app.get('/', function(req, res){
        return res.json({
            user: "Gustavo Silva",
            curso: "Omnistack 11",
