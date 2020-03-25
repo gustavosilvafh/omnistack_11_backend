d33d96b41dcec2d42ffef7655a438bb65fee5ac6
Gustavo Silva <gustavo@MacBook-Pro-de-Gustavo.local>
2020-03-25 10:43:09 -0300

    fix

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,6 @@ const express = require('express');
 
 const OngController = require('./controllers/OngController');
 
-const connection = require('./database/connection');
-
 const routes = express.Router();
 
 routes.get('/ongs', OngController.index);
